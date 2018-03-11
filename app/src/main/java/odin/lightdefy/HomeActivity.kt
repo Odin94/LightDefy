f9ba62845300098f226f67c0a001beb3c2c73c26
Odin <andreas.kammerloher@arcor.de>
2018-03-11 22:47:30 +0100

    Add basic interface layout

---
+++ app/src/main/java/odin/lightdefy/HomeActivity.kt
@@ -0,0 +1,56 @@
+package odin.lightdefy
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import kotlinx.android.synthetic.main.lightbulb_list_elem.view.*
+
+class HomeActivity : AppCompatActivity() {
+
+    private val lightbulbs = mutableListOf<Lightbulb>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        lightbulbs.add(Lightbulb("Lightbulb 1 (on, con)", true, true))
+        lightbulbs.add(Lightbulb("Lightbulb 2 (on, disc)", true, false))
+        lightbulbs.add(Lightbulb("Lightbulb 3 (off, con)", false, true))
+
+
+        val lightbulbsListView = findViewById<ListView>(R.id.lightbulbListView)
+        lightbulbsListView.adapter = LightbulbAdapter(this, lightbulbs)
+    }
+
+    private class LightbulbAdapter(val context: Context, val lightbulbs: MutableList<Lightbulb>) : BaseAdapter() {
+        override fun getView(position: Int, p1: View?, p2: ViewGroup?): View {
+            val lightbulb = getItem(position)
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val view = inflater.inflate(R.layout.lightbulb_list_elem, null)
+
+            view.name.text = lightbulb.name
+            view.lightSwitch.text = if (lightbulb.on) "Turn Off" else "Turn On"
+
+            val bulbImage = if (lightbulb.connected) {
+                if (lightbulb.on) R.drawable.light_on else R.drawable.light_off
+            } else {
+                R.drawable.light_disconnected
+            }
+            view.bulb_image.setImageResource(bulbImage)
+
+            return view;
+        }
+
+        override fun getItem(position: Int) = lightbulbs[position]
+
+        override fun getItemId(position: Int) = position.toLong()
+
+        override fun getCount() = lightbulbs.size
+
+    }
+}
---
+++ app/src/main/java/odin/lightdefy/Lightbulb.kt
@@ -0,0 +1,5 @@
+package odin.lightdefy
+
+// TODO: put image assets in here? Maybe a map of status -> image?
+
+class Lightbulb(val name: String, var on: Boolean, var connected: Boolean)
